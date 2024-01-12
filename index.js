454cbc3be1e933e2b466ad91589596714969df70
wyrla <111397148+wyrla@users.noreply.github.com>
2024-01-11 22:43:19 -0300

    chore: set db location with prompt

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 const sqlite3 = require('sqlite3').verbose();
 const crc32 = require('crc32');
 const fs = require('fs');
+const prompt = require('prompt-sync')();
+let dbLocation = process.env.HXCORE_DB || process.argv[2]
 
 const keycodes = require('./keycodes');
-
 const serializeSequence = require('./serializeSequence');
 
-let dbFilePath = process.argv[2];
+while (!dbLocation) {
+    dbLocation = prompt('Informar anne pro databse location path: ')
+}
 
-if (!dbFilePath) {
-    console.log(`Invalid db file path value ${dbFilePath} present! Exiting...`);
+if (!dbLocation) {
+    console.log(`Invalid db file path value ${dbLocation} present! Exiting...`);
     process.exit(1);
 }
 
-const db = new sqlite3.Database(dbFilePath);
+const db = new sqlite3.Database(dbLocation);
 
 let startId = 69;
 
 let stringsFilePath = process.argv[3];
 
 if (!stringsFilePath)  {
-    console.log(`Invalid strings file path value ${dbFilePath} present! Exiting...`);
+    console.log(`Invalid strings file path value ${dbLocation} present! Exiting...`);
     process.exit(1);
 }
 
