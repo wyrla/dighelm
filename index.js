ce3596328fb0040f1aacf17a48812f876bb1f9e9
wyrla <111397148+wyrla@users.noreply.github.com>
2024-01-27 23:29:51 -0300

    feat: add capitalize support and autoincrement id

--- index.js
+++ index.js
@@ -1,86 +1,124 @@
-const sqlite3 = require('sqlite3').verbose();
-const crc32 = require('crc32');
-const fs = require('fs');
-const prompt = require('prompt-sync')();
-let dbLocation = process.env.HXCORE_DB || process.argv[2]
+const sqlite3 = require("sqlite3").verbose();
+const crc32 = require("crc32");
+const fs = require("fs");
+let dbLocation = process.env.HXCORE_DB || process.argv[2];
 
-const keycodes = require('./keycodes');
-const serializeSequence = require('./serializeSequence');
-
-while (!dbLocation) {
-    dbLocation = prompt('Informar anne pro databse location path: ')
-}
+const keys = require("./keys");
+const serializeSequence = require("./serializeSequence");
 
 if (!dbLocation) {
-    console.log(`Invalid db file path value ${dbLocation} present! Exiting...`);
-    process.exit(1);
+  console.log(`Invalid db file path value ${dbLocation} present! Exiting...`);
+  process.exit(1);
 }
 
-const db = new sqlite3.Database(dbLocation);
+let strings = [process.argv[3]];
+const commandArgument = strings;
 
-let startId = 69;
-
-let stringsFilePath = process.argv[3];
-
-if (!stringsFilePath)  {
-    console.log(`Invalid strings file path value ${dbLocation} present! Exiting...`);
+if (commandArgument.includes(".txt")) {
+  if (!stringsFilePath) {
+    console.log(
+      `Invalid strings file path value ${dbLocation} present! Exiting...`
+    );
     process.exit(1);
-}
+  }
 
-let strings;
-
-try {
-    strings = fs.readFileSync(stringsFilePath, 'utf8').split('\n').map(s => s.trim()).filter(s => !!s);
-} catch (e) {
+  try {
+    strings = fs
+      .readFileSync(stringsFilePath, "utf8")
+      .split("\n")
+      .map((s) => s.trim())
+      .filter((s) => !!s);
+  } catch (e) {
     console.log(e);
     process.exit(1);
+  }
 }
 
 const macros = [];
 
+const returnStringSequence = (string) => {
+  let downTime = 0;
+  let upTime = 25;
+  return [...string]
+    .map((letter) => keys[letter])
+    .flat()
+    .map((letter) => {
+      downTime += 25;
+      upTime += 25;
+      return {
+        keyCode: letter.value,
+        downTime,
+        upTime,
+      };
+    });
+};
+
 for (const string of strings) {
-    console.log(string)
-    const sequence = [];
+  const keyCodeRef = returnStringSequence(string);
 
-    let downTime = 0;
-    let upTime = 25;
-
-    for (const letter of string) {
-        sequence.push({
-            keyCode: keycodes[letter.toUpperCase()].value,
-            downTime,
-            upTime
-        })
-
-        downTime += 25;
-        upTime += 25;
-    }
-
-    macros.push({
-        id: startId,
-        name: string,
-        sequence
-    })
-
-    startId += 1;
+  macros.push({
+    name: string,
+    sequence: keyCodeRef,
+  });
 }
 
+function connectToDatabase() {
+  return new Promise((resolve, reject) => {
+    const db = new sqlite3.Database(dbLocation);
+    db.serialize(() => {
+      resolve(db);
+    });
 
+    db.on("error", (error) => {
+      reject(error);
+    });
+  });
+}
 
-db.serialize(function () {
+function insertMacro(id, crc, name, serializedSequence, db) {
+  return new Promise((resolve, reject) => {
+    db.run(
+      `INSERT INTO kbd_macro_new (id, group_id, icon, uuid, name, macro_value) values ('${
+        id + 1
+      }', '0', '0', '${crc}', '${name}', '${serializedSequence}')`,
+      {},
+      (err) => {
+        if (err) reject(err);
+        resolve(this.lastID);
+      }
+    );
+  });
+}
+
+function getLastId(db) {
+  return new Promise((resolve, reject) => {
+    db.get(
+      `SELECT * FROM kbd_macro_new WHERE id=(SELECT max(id) FROM kbd_macro_new)`,
+      (err, row) => {
+        if (err) reject(err);
+        resolve(row);
+      }
+    );
+  });
+}
+
+(async () => {
+  try {
+    const db = await connectToDatabase();
+
     for (const macro of macros) {
-        const {id, name, sequence} = macro;
+      const { name, sequence } = macro;
 
-        const serializedSequence = JSON.stringify(serializeSequence(sequence));
+      const serializedSequence = JSON.stringify(serializeSequence(sequence));
 
-        const crc = crc32(serializedSequence);
+      const crc = crc32(serializedSequence);
+      let { id } = await getLastId(db);
 
-        db.run(`INSERT INTO kbd_macro_new (id, group_id, icon, uuid, name, macro_value) values ('${id}', '0', '0', '${crc}', '${name}', '${serializedSequence}')`, {}, err => {
-            if (err && err.code !== 'SQLITE_CONSTRAINT') {
-                console.log(err)
-            }
-        })
+      await insertMacro(id, crc, name, serializedSequence, db);
     }
-});
 
-db.close();
+    db.close();
+  } catch (error) {
+    console.error(error);
+  }
+})();
